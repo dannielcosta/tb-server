f64e0a424d4d59fe6887951c8afcc13674c0c173
Daniel_Costa <dan.costaa.12@gmail.com>
2024-05-09 15:19:23 +0100

    ironlauncher

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
+const app = require("./app");
 
-const express = require("express");
-const app = express();
-const expressLayouts = require("express-ejs-layouts");
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
+const PORT = process.env.PORT || 5005;
 
-const indexRouter = require("./routes/index");
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
-
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
-app.use('/', indexRouter);
-
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
